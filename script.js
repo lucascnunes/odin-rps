6553cfecfaf21dddbc7b334a4cce7f54e77d3ebc
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2023-11-10 18:28:44 -0300

    defining variables

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let round = 1;
+let playerPoints = 0;
+let computerPoints = 0;
+let playerChoice = "";
+
+console.log("Type game() on the console to start Rock, Paper and Scissors game!");
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 

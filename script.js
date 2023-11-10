4c151473e0d616f0924d2010d0c9832b20068e41
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2023-11-10 18:29:16 -0300

    defining playRound function and results

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    console.log("The computer picked " + computerSelection);
+    if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        playerPoints += 1;
+        return "You win! Rock beats Scissors!!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        playerPoints += 1;
+        return "You win! Scissors beats Paper!!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        playerPoints += 1;
+        return "You win! Paper beats Rock!!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+        computerPoints += 1;
+        return "You lose! Paper beats Rock!!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
+        computerPoints += 1;
+        return "You lose! Scissors beats Paper!!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+        computerPoints += 1;
+        return "You lose! Rock beats Scissors!!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
+        return "Draw!!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
+        return "Draw!!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
+        return "Draw!!";
+    }
+}

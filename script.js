de2aba6f0e3979434f8bd033e42bb2457bbfac8b
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2023-11-10 22:01:12 -0300

    now with an ui

--- script.js
+++ script.js
@@ -1,96 +1,60 @@
-let round = 1;
-let playerPoints = 0;
-let computerPoints = 0;
 let playerChoice = "";
 
-console.log("Type game() on the console to start Rock, Paper and Scissors game!");
+const message = document.querySelector("#message");
+const choices = document.querySelectorAll("button");
+choices.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerChoice = button.id;
+        playRound(playerChoice, getComputerChoice());
+    });
+});
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
     if (choice === 0) {
-        // rock
+        document.querySelector("#pc-rock").classList.add("active");
         return "Rock";
     } else if (choice == 1) {
-        // paper
+        document.querySelector("#pc-paper").classList.add("active");
         return "Paper";
     } else {
-        // scissors
+        document.querySelector("#pc-scissors").classList.add("active");
         return "Scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log("The computer picked " + computerSelection);
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        playerPoints += 1;
-        return "You win! Rock beats Scissors!!";
+        message.innerHTML = "You win! Rock beats Scissors!!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        playerPoints += 1;
-        return "You win! Scissors beats Paper!!";
+        message.innerHTML = "You win! Scissors beats Paper!!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        playerPoints += 1;
-        return "You win! Paper beats Rock!!";
+        message.innerHTML = "You win! Paper beats Rock!!";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        computerPoints += 1;
-        return "You lose! Paper beats Rock!!";
+        message.innerHTML = "You lose! Paper beats Rock!!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-        computerPoints += 1;
-        return "You lose! Scissors beats Paper!!";
+        message.innerHTML = "You lose! Scissors beats Paper!!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-        computerPoints += 1;
-        return "You lose! Rock beats Scissors!!";
+        message.innerHTML = "You lose! Rock beats Scissors!!";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
-        return "Draw!!";
+        message.innerHTML = "Draw!!";
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
-        return "Draw!!";
+        message.innerHTML = "Draw!!";
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
-        return "Draw!!";
+        message.innerHTML = "Draw!!";
     }
-}
-function game() {
-    // debugger;
-    playerChoice = prompt("Let's play Rock, Paper, Scissors! Pick one option:");
 
-    if (
-        playerChoice.toLowerCase() === "rock" ||
-        playerChoice.toLowerCase() === "paper" ||
-        playerChoice.toLowerCase() === "scissors"
-    ) {
-        //
-        while (round <= 5) {
-            if (round !== 1) playerChoice = prompt("Let's play Rock, Paper, Scissors! Pick one option:");
+    message.classList.remove("hidden");
+    setTimeout(() => {
+        document.querySelector("#pc-rock").classList.remove("active");
+        document.querySelector("#pc-paper").classList.remove("active");
+        document.querySelector("#pc-scissors").classList.remove("active");
 
-            console.log("Round " + round + " out of 5:");
-            console.log("You've picked " + playerChoice);
-            console.log(playRound(playerChoice, getComputerChoice()));
-            round++;
-        }
-
-        console.log("End of the game!");
-
-        // verify who won
-        if (playerPoints === computerPoints) {
-            console.log("You two draw with " + playerPoints + " points!");
-        } else if (playerPoints > computerPoints) {
-            console.log("You won with " + playerPoints + " points!");
-        } else {
-            console.log("The computer won with " + computerPoints + " points!");
-        }
-
-        // restart
-        resetGame();
-    } else {
-        console.log("Please pick one of the options: Rock, Paper or Scissors");
-        // restart
-        resetGame();
-    }
+        message.classList.add("hidden");
+    }, 800);
 }
 
 function resetGame() {
-    round = 1;
-    playerPoints = 0;
-    computerPoints = 0;
-    playerChoice = null;
-    console.log("Type game() on the console to start Rock, Paper and Scissors game!");
+    playerChoice = "";
 }
